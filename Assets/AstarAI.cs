73e0c9ca474c85f17e01d197240458f053a96257
Arran <arran_ford@live.com>
2013-10-03 02:43:54 +0800

    Began adding pathfinding

--- Assets/AstarAI.cs
+++ Assets/AstarAI.cs
@@ -9,12 +9,12 @@ public class AstarAI : MonoBehaviour {
     
     private Seeker seeker;
     private CharacterController controller;
- 
+    public Vector3 Temp;
     //The calculated path
     public Path path;
     
     //The AI's speed per second
-    public float speed = 100;
+    public float speed = 1000;
     
     //The max distance from the AI to a waypoint for it to continue to the next waypoint
     public float nextWaypointDistance = 3;
@@ -72,7 +72,7 @@ Instantiate (particle, hit.point, transform.rotation);
 				targetPosition = hit.point;
 			}
 
-			
+			targetPosition.y = 1.08f;
           Start(); // gameObject.renderer.material.color = Color.red;
         }
 	}
@@ -106,9 +106,14 @@ Instantiate (particle, hit.point, transform.rotation);
         }
         
         //Direction to the next waypoint
-        Vector3 dir = (path.vectorPath[currentWaypoint]-transform.position).normalized;
+		
+		Temp = path.vectorPath[currentWaypoint];
+		//Temp.y +=2.08f;
+        Vector3 dir = (Temp-transform.position).normalized;
         dir *= speed * Time.fixedDeltaTime;
         controller.SimpleMove (dir);
+		//controller.
+		//transform.position = path.vectorPath[currentWaypoint];
         
         //Check if we are close enough to the next waypoint
         //If we are, proceed to follow the next waypoint
